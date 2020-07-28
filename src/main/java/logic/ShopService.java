0ec95f73e022ad33084e3298a667c3e88ca9e16a
seyeon-shijuan <66824510+seyeon-shijuan@users.noreply.github.com>
2020-07-28 18:28:35 +0900

    0728_admin_2

--- src/main/java/logic/ShopService.java
+++ src/main/java/logic/ShopService.java
@@ -1,6 +1,8 @@
 package logic;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,9 +34,35 @@ public class ShopService {
 	public List<User> getUserList() {
 		return userDao.list();
 	}
-
+	
+	//[관리자] 메일 보낼 유저리스트 0728
 	public List<User> userlist(String[] idchks) {
 		return userDao.list(idchks);
 	}
+	
+	//[관리자] 그래프1 0728
+	public Map<String, Object> graph1() {
+		// TODO Auto-generated method stub
+		Map<String,Object> map = new HashMap<String,Object>();
+//		for(Map<String,Object> m : boardDao.graph1()) {
+//			map.put((String)m.get("name"), m.get("cnt"));
+//		}
+		return map;
+	}
+	
+	//[관리자] 유저리스트 가져오기 0728
+	public List<User> list() {
+		// TODO Auto-generated method stub
+		return userDao.list();
+		}
 
+	//[관리자] 그래프2 0728
+			public Map<String, Object> graph2() {
+				// TODO Auto-generated method stub
+				Map<String,Object> map = new HashMap<String,Object>();
+//				for(Map<String,Object> m : boardDao.graph2()) {
+//					map.put((String)m.get("regdate"), m.get("cnt"));
+//				}
+				return map;
+			}
 }
