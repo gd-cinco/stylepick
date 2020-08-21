165e1de711dac1e2445022c48b5080488f683224
LeeGihwan2 <66305035+LeeGihwan2@users.noreply.github.com>
2020-08-21 17:38:15 +0900

    [cart] checkout

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -1,15 +1,48 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import logic.Cart;
+import logic.Item;
+import logic.ItemSet;
+import logic.Sale;
+import logic.ShopService;
+import logic.User;
 
 @Controller
 @RequestMapping("cart")
 public class CartController {
 
+	@Autowired
+	private ShopService service;
 	
 	@RequestMapping("*")	//item/list.shop
 	public String cartview() {
 		return null;
+	}
+	
+	@RequestMapping("end")
+	public ModelAndView checkend(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+//		Cart cart = (Cart)session.getAttribute("CART");
+		Cart cart = new Cart();
+		Item tItem = new Item();
+		tItem.setCode("1");
+		ItemSet tItemSet = new ItemSet(tItem, 2);
+		cart.push(tItemSet);
+		User loginUser = (User)session.getAttribute("loginUser");
+		Sale sale = service.checkend(loginUser, cart);
+		long total = cart.getTotal();
+		// 주문완료시 장바구니 내용 제거
+//		session.removeAttribute("CART");
+//		mav.addObject("sale", sale);
+//		mav.addObject("total", total);
+		mav.setViewName("http://localhost:8080/stylepick/cart/pay.shop");
+		return mav;
 	}
 }
--- src/main/java/logic/ShopService.java
+++ src/main/java/logic/ShopService.java
@@ -18,6 +18,8 @@ import dao.BoardDao;
 import dao.CommentDao;
 import dao.ItemDao;
 import dao.LineDao;
+import dao.SaleDao;
+import dao.SaleItemDao;
 import dao.UserDao;
 
 @Service	//@Component + service ( Controller와 dao의 중간)
@@ -46,6 +48,12 @@ public class ShopService {
 	
 	@Autowired
 	private LineDao lineDao;
+	
+	@Autowired
+	private SaleDao saleDao;
+	
+	@Autowired
+	private SaleItemDao saleItemDao;
  
 	//[user] 입력창 중복확인
 	public int joincompare(String key, String val) {
@@ -438,6 +446,27 @@ public class ShopService {
 		int max = boardDao.maxno();
 		board.setNo(++max);
 		boardDao.insert(board);
+	}
+
+	public Sale checkend(User loginUser, Cart cart) {
+		Sale sale = new Sale();
+		int maxno = saleDao.getMaxSaleid();
+		sale.setSaleid(++maxno);
+		sale.setUser(loginUser);
+		sale.setUserid(loginUser.getUserid());
+		
+		saleDao.insert(sale);
+		// 장바구니 판매 상품 정보
+		List<ItemSet> itemList = cart.getItemSetList();
+		int seq = 0;
+		for(ItemSet itemSet : itemList) {
+			++seq;
+			SaleItem saleItem = new SaleItem(sale.getSaleid(), seq, itemSet);
+			sale.getItemList().add(saleItem);
+			saleItemDao.insert(saleItem);
+		}
+		
+		return sale;
 	}
 
 
